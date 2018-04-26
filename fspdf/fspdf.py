141afbaa058015bb5502871a7649fc49db03f4a8
Kai Eckert <hallo@kaiec.de>
2018-04-26 10:16:42 +0200

    Selection, adjustable line height

--- fspdf/fspdf.py
+++ fspdf/fspdf.py
@@ -4,7 +4,7 @@
 """fspdf.fspdf: Fill and sign PDFs."""
 
 
-__version__ = "0.1.1"
+__version__ = "0.1.2-dev"
 
 
 import tkinter as tk
@@ -25,12 +25,29 @@ class Page():
         self.height = self.img.height
         self.rel_width = 1
         self.annotations = []
+        self.selected = None
         self.canvas = canvas
         self.xoffset = (self.canvas.winfo_width() - self.width) / 2
         self.yoffset = (self.canvas.winfo_height() - self.height) / 2
         self.tkimg = ImageTk.PhotoImage(image=self.img)
         self.box_resize(self.canvas.winfo_width(),
                         self.canvas.winfo_height())
+
+    def select(self, annotation):
+        if annotation == self.selected:
+            return
+        if self.selected is not None:
+            self.selected.deselect()
+        self.selected = annotation
+        self.selected.update_select_rect()
+        print("New selection: {}".format(self.selected))
+
+    def deselect(self):
+        if self.selected is not None:
+            old = self.selected
+            self.selected = None
+            old.update_select_rect()
+        print("Deselected: {}".format(old))
 
     def box_resize(self, width, height):
         sw = width / self.width
@@ -76,6 +93,7 @@ class Annotation():
         # this data is used to keep track of an
         # item being dragged
         self._drag_data = {"x": 0, "y": 0}
+        self.select_rect_oid = None
         if event is not None:
             self.x = event.x
             self.y = event.y
@@ -83,9 +101,20 @@ class Annotation():
             self.rel_y = (self.y - page.yoffset) / page.height
             self.page.canvas_draw()
 
+    def is_selected(self):
+        return self.page.selected == self
+
+    def select(self):
+        self.page.select(self)
+
+    def deselect(self):
+        if self.is_selected():
+            self.page.deselect()
+
     def delete(self, event):
         self.page.canvas.delete(tk.CURRENT)
         self.page.annotations.remove(self)
+        self.deselect()
 
     def resize(self, width):
         if width > self.page.img.width:
@@ -122,7 +151,31 @@ class Annotation():
                       self.y,
                       self.oid,
                       self.width))
+        self.update_select_rect()
         self.page.canvas.update_idletasks()
+
+    def coords(self):
+        return (int(self.x-self.img.width//2),
+                int(self.y-self.img.height//2),
+                int(self.x+self.img.width//2),
+                int(self.y+self.img.height//2))
+
+    def update_select_rect(self):
+        if self.select_rect_oid not in self.page.canvas.find_all():
+            self.select_rect_oid = None
+        if self.is_selected():
+            if self.select_rect_oid is not None:
+                self.page.canvas.coords(self.select_rect_oid, *self.coords())
+                print("Update selection rectangle at {}".format(self.coords()))
+            else:
+                self.select_rect_oid = self.page.canvas.create_rectangle(
+                                            *self.coords())
+                print("Draw new selection at {}".format(self.coords()))
+        else:
+            if self.select_rect_oid is not None:
+                self.page.canvas.delete(self.select_rect_oid)
+                self.select_rect_oid = None
+                print("Removed selection rectangle for {}".format(self))
 
     def image_draw(self, image):
         x = int(self.rel_x * image.width)
@@ -137,16 +190,19 @@ class Annotation():
     def smaller(self, event):
         self.resize(self.img.width * 0.9)
         self.page.canvas.itemconfig(tk.CURRENT, image=self.tkimg)
+        self.update_select_rect()
         self.page.canvas.update_idletasks()
 
     def larger(self, event):
         self.resize(self.img.width * 1.2)
         self.page.canvas.itemconfig(tk.CURRENT, image=self.tkimg)
+        self.update_select_rect()
         self.page.canvas.update_idletasks()
 
     def drag_start(self, event):
         '''Begining drag of an object'''
         event.widget.tag_click = True
+        self.select()
         # record the item and its location
         self._drag_data["x"] = event.x
         self._drag_data["y"] = event.y
@@ -171,6 +227,7 @@ class Annotation():
         delta_y = event.y - self._drag_data["y"]
         # move the object the appropriate amount
         self.page.canvas.move(tk.CURRENT, delta_x, delta_y)
+        self.update_select_rect()
         self.x = self.x + delta_x
         self.y = self.y + delta_y
         # record the new position
@@ -307,17 +364,25 @@ class Fspdf:
             if value == "":
                 print("No text, returning...")
                 return
+            lines = value.split("\n")
             fnt = ImageFont.truetype('Pillow/Tests/fonts/DejaVuSans.ttf', 72)
             txt = Image.new('RGBA', (1000, 1000), (0, 0, 0, 0))
             d = ImageDraw.Draw(txt)
-            size = d.textsize(value, font=fnt)
-            if size[0] == 0 or size[1] == 0:
+            width = 0
+            height = 0
+            for l in lines:
+                size = d.textsize(l, font=fnt)
+                width = max(size[0], width)
+                height = max(size[1], height)
+            if width == 0 or height == 0:
                 print("Fill image is empty, returning")
                 return
+            size = (width, int(height * 1.5 * len(lines)))
             txt = Image.new('RGBA', size, (0, 0, 0, 0))
             d = ImageDraw.Draw(txt)
-            print("Size of text: {}".format(size))
-            d.text((0, 0), value, font=fnt, fill=(0, 0, 0, 255))
+            print("Size of text: {}".format((width, size)))
+            for i, l in enumerate(lines):
+                d.text((0, i * height * 1.5), l, font=fnt, fill=(0, 0, 0, 255))
             Annotation(self.page, txt, event)
 
     def prev_page(self):
